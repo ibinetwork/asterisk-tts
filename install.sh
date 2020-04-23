50fcef394dacee7a8c5be2807634996cb63b4b42
Rafael Tavares <rafael@ibinetwork.com.br>
2020-04-22 21:29:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,4 +13,6 @@ cowsay "INSTALANDO WATSON E GOOGLE TTS"
 cd /usr/src/
 git clone https://github.com/ibinetwork/asterisk-tts.git
 cp asterisk-tts/agi-bin/*.php /var/lib/asterisk/agi-bin
+mkdir /var/lib/asterisk/sounds/tts/
+chown -Rf asterisk.asterisk /var/lib/asterisk/sounds/tts/
 amportal chown
